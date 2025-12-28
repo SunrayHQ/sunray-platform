dae7d8e61825554a5f02fdd5a1281f0a03473cba
SunrayHQ <gavinsebby630@gmail.com>
2025-12-28 10:34:50 -0600

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Sunray</h1>
+      <p>The platform is live.</p>
+    </main>
+  );
+}
